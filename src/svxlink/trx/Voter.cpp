5dd7d013288e87ee1b5e122da5085d59fb9c5ff7
Wim Fournier <github@fournier.nl>
2023-03-14 19:21:33 +0100

    also print it

--- src/svxlink/trx/Voter.cpp
+++ src/svxlink/trx/Voter.cpp
@@ -723,12 +723,13 @@ void Voter::printSquelchState(void)
     char rx_id = srx->id();
     rx["id"] = std::string(&rx_id, &rx_id+1);
     rx["enabled"] = is_enabled;
-    if (is_enabled)
-    {
+    // https://github.com/sm0svx/svxlink/issues/608
+    // if (is_enabled)
+    // {
       rx["sql_open"] = sql_is_open;
       rx["active"] = is_active;
       rx["siglev"] = static_cast<int>(siglev);
-    }
+    // }
     event.append(rx);
   }
   Json::StreamWriterBuilder builder;
